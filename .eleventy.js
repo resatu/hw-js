6c4d4de6a9a7dc92331aa8f53bbc80ae8e56d71e
resatugurulu <resatugur.ulu@gmail.com>
2023-09-06 10:40:20 +0300

    update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,25 @@
+// Don’t forget to `npm install sass`!
+const sass = require("sass");
+
 module.exports = function (eleventyConfig) {
-  // Output directory: _site
-
+  eleventyConfig.addTemplateFormats("scss");
   eleventyConfig.addPassthroughCopy("assets/scripts/**.*");
+  eleventyConfig.addPassthroughCopy("assets/images/**.*");
+  eleventyConfig.addPassthroughCopy("assets/fonts/**.*");
+  eleventyConfig.addPassthroughCopy("assets/icons/**.*");
+  eleventyConfig.addPassthroughCopy("assets", "assets");
+  // Creates the extension for use
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css", // optional, default: "html"
 
+    // `compile` is called once per .scss file in the input directory
+    compile: async function (inputContent) {
+      let result = sass.compileString(inputContent);
+
+      // This is the render function, `data` is the full data cascade
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
 };
